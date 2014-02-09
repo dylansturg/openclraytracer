1afe8a9abcd334141d98c7a3d3c703964ab26a78
xinyu1991 <chengx@rose-hulman.edu>
2014-02-09 17:32:38 -0500

    ppm writer

---
+++ ParallelSumOpenCL/ParallelSumOpenCL/buffer.h
@@ -0,0 +1,38 @@
+#ifndef __BUFFER
+#define __BUFFER
+#include "GenVector.h"
+
+class Buffer
+{
+public:
+	Buffer(unsigned int w, unsigned int h)
+	{
+		this->w = w;
+		this->h = h;
+		alloc();
+	}
+
+	Color & at(unsigned int x, unsigned int y)
+	{
+		return data[x+y*this->w];
+	}
+
+	unsigned int getW(){
+		return this->w;
+	}
+
+	unsigned int getH(){
+		return this->h;
+	}
+
+private:
+	unsigned int w;
+	unsigned int h;
+	Color * data;
+
+	void alloc()
+	{
+		data = (Color *)malloc(this->h*this->w*sizeof(Color));
+	}
+};
+#endif
--- ParallelSumOpenCL/ParallelSumOpenCL/main.cpp
+++ ParallelSumOpenCL/ParallelSumOpenCL/main.cpp
@@ -17,12 +17,30 @@
 #include "CStyleHelloWorld.h"
 #include "Scene.h"
 #include "objLoader.h"
+#include "buffer.h"
+#include "simplePPM.h"
+
+#define RES 200
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	Scene scene;
+	Buffer buffer = Buffer(RES, RES);
+
+
+	for (int y = 1; y <= RES; y++)
+	{
+		for (int x = 0; x < RES; x++)
+		{
+			Color c = Color(x, y, (x + y) % 255);
+			buffer.at(x, RES - y) = c;
+		}
+	}
+
+	simplePPM_write_ppm("ray.ppm", RES, RES, (unsigned char *)&buffer.at(0, 0));
+
 	return CStyleHelloWorld();
 }
 
